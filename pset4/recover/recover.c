03d0166cb5307ea01c91b12da312cf966fbbe608
Denis Ignashov <ignashov.denis@gmail.com>
2016-07-02 23:09:39 +0300

    Update recover.c
    Fix bugs. Now it is works.

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,12 +1,3 @@
-/**
- * recover.c
- *
- * Computer Science 50
- * Problem Set 4
- *
- * Recovers JPEGs from a forensic image.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -15,20 +6,12 @@
 // size of memory block
 #define blockSize 512
 
+// length of outfile names
 #define titleLength 8
 
-void defFileName(char* title, uint8_t num)
-{
-    if((num / 10) == 0) 
-    {
-        sprintf(title, "%i%i%i.jpg", 0, 0, num);
-    }
-    else 
-    {
-        sprintf(title, "%i%i.jpg", 0, num);
-    }
-    return;
-}
+// define outfile names
+void defFileName(char* title, uint8_t num);
+
 
 int main(int argc, char* argv[])
 {
@@ -47,29 +30,42 @@ int main(int argc, char* argv[])
         return 2;
     }
     
+    // identify the first characters of jpg
     uint8_t jpgHead1[4] = {0xff, 0xd8, 0xff, 0xe0};
     uint8_t jpgHead2[4] = {0xff, 0xd8, 0xff, 0xe1};
     
-    char* title = malloc(titleLength);
+    // pointer to the outfiles
     FILE* outptr;
+    
+    // the names of the output files
+    char* title = malloc(titleLength);
+    
+    // number of current outfile
     uint8_t count = 0;
+    
+    // buffer memory for storing blocks
     uint8_t* buffer = malloc(blockSize);
     
+    // ignoring garbage
     do
     {
         fread(buffer, blockSize, 1, inptr);
     }
     while(memcmp(jpgHead1, buffer, 4) && memcmp(jpgHead2, buffer, 4));
     
+    // read card and create result files
     do
     {
+        // write the first block of file
         if (!(memcmp(jpgHead1, buffer, 4) && memcmp(jpgHead2, buffer, 4)))
         {
+            // it works in each iteration except the first
             if (count!=0) 
             {
                 fclose(outptr);
             }
             
+            // create the new file for image
             defFileName(title, count++);
             outptr = fopen(title, "w");
             if (outptr == NULL)
@@ -77,8 +73,10 @@ int main(int argc, char* argv[])
                 printf("Could not open new file for write.\n");
                 return 3;
             }
+            
             fwrite(buffer, blockSize, 1, outptr);
         }
+        // wtite the other blocks
         else 
         {
             fwrite(buffer, blockSize, 1, outptr);
@@ -86,11 +84,26 @@ int main(int argc, char* argv[])
         
     } while (fread(buffer, blockSize, 1, inptr));
     
+    // frees memory
     free(title);
     free(buffer);
     
+    // close infile and outfile
     fclose(inptr);
     fclose(outptr);
     
     return 0;
+}
+
+void defFileName(char* title, uint8_t num)
+{
+    if((num / 10) == 0) 
+    {
+        sprintf(title, "%i%i%i.jpg", 0, 0, num);
+    }
+    else 
+    {
+        sprintf(title, "%i%i.jpg", 0, num);
+    }
+    return;
 }
